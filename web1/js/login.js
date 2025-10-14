844966acca314e6b8079d0ff0c1de2755abe626b
Phạm Phước Vĩnh Quang <phamquang10102006@gmail.com>
2025-10-14 16:00:03 +0700

    Create login.js

---
+++ web1/js/login.js
@@ -0,0 +1,31 @@
+const accounts = [
+    { username: "admin", password: "123456"}
+];
+const form = document.getElementById("loginForm");
+const usernameInput = document.getElementById("username");
+const passwordInput= document.getElementById("password");
+const error1 = document.getElementById("loginError");
+ form.addEventListener("submit", function(test) {
+test.preventDefault();
+const username = usernameInput.value.trim();
+const password = passwordInput.value.trim();
+    const found= accounts.find(
+        acc => acc.username=== username &&  acc.password === password
+    )
+    if (found)
+    {
+        alert("Đăng nhập thành công!");
+        window.location.href= "infor_change.html";
+
+    }
+    else
+    {
+        passwordInput.value="";
+        passwordInput.focus();
+        error1.textContent="Nhập sai tên người dùng hoặc mật khẩu!";
+        error1.style.display="block";
+    }
+ }
+);
+usernameInput.addEventListener("input", ()=> error1.style.display="none");
+passwordInput.addEventListener("input", () => error1.style.display="none");
